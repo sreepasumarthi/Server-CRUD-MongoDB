00b97ddc17e3c4da419f582972c6c242f8b742c1
Sree <sreepasumarthi21@gmail.com>
2024-04-13 16:04:08 -0400

    stuff

--- server.js
+++ server.js
@@ -10,129 +10,130 @@ app.use(cors());
 const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "./public/images/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+    destination: (req, file, cb) => {
+        cb(null, "./public/images/");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
 
 const upload = multer({ storage: storage });
 
 mongoose
-  .connect(
-    "mongodb+srv://portiaportia:E8AUfgE3717JFt0y@data.ng58qmq.mongodb.net/"
-  )
-  .then(() => {
-    console.log(" Successfully connected to mongodb!");
-  })
-  .catch((error) => {
-    console.log("Sorry, couldn't connect to mongodb.", error);
-  });
+    .connect(
+        "mongodb+srv://sreepasumarthi:coolgoal@cluster0.hvvzzie.mongodb.net/"
+    )
+    .then(() => {
+        console.log("Successfully connected to mongodb!");
+    })
+    .catch((error) => {
+        console.log("Sorry, couldn't connect to mongodb.", error);
+    });
+    
 const craftSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  supplies: [String],
-  img: String,
+    name: String,
+    description: String,
+    supplies: [String],
+    img: String,
 });
 
 const Craft = mongoose.model("Craft", craftSchema);
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/api/crafts", (req, res) => {
-  getCrafts(res);
+    getCrafts(res);
 });
 
 const getCrafts = async (res) => {
-  const crafts = await Craft.find();
-  res.send(crafts);
+    const crafts = await Craft.find();
+    res.send(crafts);
 };
 
 app.get("/api/crafts/:id", (req, res) => {
-  getCraft(req.params.id, res);
+    getCraft(req.params.id, res);
 });
 
 const getCraft = async (id, res) => {
-  const craft = await Craft.findOne({ _id: id });
-  res.send(craft);
+    const craft = await Craft.findOne({ _id: id });
+    res.send(craft);
 };
 
 app.post("/api/crafts", upload.single("img"), (req, res) => {
-  const result = validateCraft(req.body);
-  console.log(result);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
+    const result = validateCraft(req.body);
+    console.log(result);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
 
-  const craft = new Craft({
-    name: req.body.name,
-    description: req.body.description,
-    supplies: req.body.supplies.split(","),
-  });
+    const craft = new Craft({
+        name: req.body.name,
+        description: req.body.description,
+        supplies: req.body.supplies.split(","),
+    });
 
-  if (req.file) {
-    craft.img = "images/" + req.file.filename;
-  }
+    if (req.file) {
+        craft.img = "images/" + req.file.filename;
+    }
 
-  createCraft(craft, res);
+    createCraft(craft, res);
 });
 
 const createCraft = async (craft, res) => {
-  const result = await craft.save();
-  res.send(craft);
+    const result = await craft.save();
+    res.send(craft);
 };
 
 app.put("/api/crafts/:id", upload.single("img"), (req, res) => {
-  const result = validateCraft(req.body);
-  console.log(result);
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
-  }
-  updateCraft(req, res);
+    const result = validateCraft(req.body);
+    console.log(result);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    updateCraft(req, res);
 });
 
 const updateCraft = async (req, res) => {
-  let fieldsToUpdate = {
-    name: req.body.name,
-    description: req.body.description,
-    supplies: req.body.supplies.split(","),
-  };
+    let fieldsToUpdate = {
+        name: req.body.name,
+        description: req.body.description,
+        supplies: req.body.supplies.split(","),
+    };
 
-  if (req.file) {
-    fieldsToUpdate.img = "images/" + req.file.filename;
-  }
+    if (req.file) {
+        fieldsToUpdate.img = "images/" + req.file.filename;
+    }
 
-  const result = await Craft.updateOne({ _id: req.params.id }, fieldsToUpdate);
+    const result = await Craft.updateOne({ _id: req.params.id }, fieldsToUpdate);
 
-  res.send(result);
+    res.send(result);
 };
 
 app.delete("/api/crafts/:id", (req, res) => {
-  removeCraft(res, req.params.id);
+    removeCraft(res, req.params.id);
 });
 
 const removeCraft = async (res, id) => {
-  const craft = await Craft.findByIdAndDelete(id);
-  res.send(craft);
+    const craft = await Craft.findByIdAndDelete(id);
+    res.send(craft);
 };
 
 function validateCraft(craft) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-    description: Joi.string().min(3).required(),
-    supplies: Joi.allow(""),
-    _id: Joi.allow(""),
-  });
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        description: Joi.string().min(3).required(),
+        supplies: Joi.allow(""),
+        _id: Joi.allow(""),
+    });
 
-  return schema.validate(craft);
+    return schema.validate(craft);
 }
 
 app.listen(3000, () => {
-  console.log("I'm listening");
+    console.log("I'm listening");
 });
